7d7a296f8f0b46acb17d09557278ca36c726919f
xunqiu2 <568928178@qq.com>
2016-05-18 13:34:32 +0800

    update all

--- src/test/java/Util/FilterDataUtilTest.java
+++ src/test/java/Util/FilterDataUtilTest.java
@@ -16,52 +16,59 @@ import java.util.regex.Pattern;
  */
 public class FilterDataUtilTest
 {
-    private String encoding = "utf-8";
-    private long [] startDate1 = {1459940640,1460458380,1460798580,1461748140};
-    private long [] endDate1 = {1459990800,1460592000,1460941200,1461805200};
-    private long [] startDate2 = {1459994400,1460599200,1460944800,1461808800};
-    private long [] endDate2 = {1460167200,1460772000,1461117600,1461981600};
-    private List []userList = new List[startDate1.length];
+    private String encoding = "utf-8";//默认读取文件使用UTF-8
+    private long [] arrStartDate1 = {1459940640,1460458380,1460798580,1461748140}; //更新前开始时间的时间戳
+    private long [] arrEndDate1 = {1459990800,1460592000,1460941200,1461805200};   //更新前结束时间的时间戳
+    private long [] arrStartDate2 = {1459994400,1460599200,1460944800,1461808800}; //更新后结束时间的时间戳
+    private long [] arrEndDate2 = {1460167200,1460772000,1461117600,1461981600};   //更新后结束时间的时间戳
+    private List [] arrUserList = new List[arrStartDate1.length];
     private String regEx = "求组|组我|组队|进组|进队|来人|随便来|" +
             "来一|来两|来二|来1|来2|来个|1来|2来|3来|4来|5来|6来|7来|8来|9来|10来|" +
             "神来|星来|来输出|来治疗|来奶|来大奶|来活人|来尸体|组上|开组|开车|上车|" +
             "司机|求带|带我|组我|加我|的加|走一个|走起|正在大荒寻找有缘人,快来结交一下吧|" +
             "开门迎客,欢迎各位天选者前来探访|[我的空间]";
 
-    private Pattern p = Pattern.compile(regEx);
+    private Pattern pattern = Pattern.compile(regEx);
     private int countAllValidReview = 0;
     private int countAllReview = 0;
     private int[] arrFileWriterCount = new int[8];
     private FileWriter fileWriter = null;
     private List<FileWriter> fileWriterList = new ArrayList<FileWriter>();
 
-    private String []allDate = new String[]{"20160406","20160407",
+    //所有日期
+    private String [] arrAllDate = new String[]{"20160406","20160407",
             "20160408","20160409","20160412","20160413","20160414",
             "20160415","20160416","20160417","20160418","20160419",
             "20160420","20160427","20160428","20160429","20160430"};
-    private String []allPartFile= new String[]{"00","01", "02","03","04","05","06",
+    //待读取数据文件格式头
+    private String [] arrAllPartFileHead = new String[]{"00","01", "02","03","04","05","06",
             "07","08","09","10","11", "12","13","14","15"};
 
-    private String []allTimePeriod= new String[]{
-        "2016-04-06-19:04:00_2016-04-07-09:00:00.txt",
-        "2016-04-07-10:00:00_2016-04-09-10:00:00.txt",
-        "2016-04-12-18:53:00_2016-04-14-08:00:00.txt",
-        "2016-04-14-10:00:00_2016-04-16-10:00:00.txt",
-        "2016-04-16-17:23:00_2016-04-18-09:00:00.txt",
-        "2016-04-18-10:00:00_2016-04-20-10:00:00.txt",
-        "2016-04-27-17:09:00_2016-04-28-09:00:00.txt",
-        "2016-04-28-10:00:00_2016-04-30-10:00:00.txt"};
+    //八个时间段
+    private String [] arrAllTimePeriod = new String[]{
+        "2016-04-06-19-04-00_2016-04-07-09-00-00.txt",
+        "2016-04-07-10-00-00_2016-04-09-10-00-00.txt",
+        "2016-04-12-18-53-00_2016-04-14-08-00-00.txt",
+        "2016-04-14-10-00-00_2016-04-16-10-00-00.txt",
+        "2016-04-16-17-23-00_2016-04-18-09-00-00.txt",
+        "2016-04-18-10-00-00_2016-04-20-10-00-00.txt",
+        "2016-04-27-17-09-00_2016-04-28-09-00-00.txt",
+        "2016-04-28-10-00-00_2016-04-30-10-00-00.txt"};
 
+    /**
+     * @Description 初始化UserList名单，八个时间段一共
+     * 有四份不同的监控用户名单，每份监控名单代表两个时间段
+     */
     public void initialUserList()
     {
-        userList[0] = FilterDataUtil.readUserAccountIDList("Data/FilteredConfig/UserList/4.6-4.9.txt");
-        userList[1] = FilterDataUtil.readUserAccountIDList("Data/FilteredConfig/UserList/4.12-4.16.txt");
-        userList[2] = FilterDataUtil.readUserAccountIDList("Data/FilteredConfig/UserList/4.16-4.20.txt");
-        userList[3] = FilterDataUtil.readUserAccountIDList("Data/FilteredConfig/UserList/4.27-4.30.txt");
-        for(int i=0;i<userList.length;i++)
+        arrUserList[0] = FilterDataUtil.readUserAccountIDList("Data/FilteredConfig/UserList/4.6-4.9.txt");
+        arrUserList[1] = FilterDataUtil.readUserAccountIDList("Data/FilteredConfig/UserList/4.12-4.16.txt");
+        arrUserList[2] = FilterDataUtil.readUserAccountIDList("Data/FilteredConfig/UserList/4.16-4.20.txt");
+        arrUserList[3] = FilterDataUtil.readUserAccountIDList("Data/FilteredConfig/UserList/4.27-4.30.txt");
+        for(int i = 0; i< arrUserList.length; i++)
         {
-            System.err.println(allTimePeriod[i*2]+"用户名单数: "+userList[i].size());
-            System.err.println(allTimePeriod[i*2+1]+"用户名单数: "+userList[i].size());
+            System.err.println(arrAllTimePeriod[i*2]+"用户名单数: "+ arrUserList[i].size());
+            System.err.println(arrAllTimePeriod[i*2+1]+"用户名单数: "+ arrUserList[i].size());
         }
     }
 
@@ -73,14 +80,14 @@ public class FilterDataUtilTest
         String inputFilePath = "Data/OriginData/dt=";
         try {
             fileWriter = new FileWriter("Data/allValidReview.txt");
-            for (int i=0;i<allTimePeriod.length;i++)
+            for (int i = 0; i< arrAllTimePeriod.length; i++)
             {
-                fileWriterList.add(new FileWriter(allTimePeriod[i]));
+                fileWriterList.add(new FileWriter(arrAllTimePeriod[i]));
             }
 
-            for(String date:allDate)
+            for(String date: arrAllDate)
             {
-                for(String partFile:allPartFile )
+                for(String partFile: arrAllPartFileHead)
                 {
                     String inputFile = inputFilePath+date+"/"+partFile+"Chat"+date+"00_"+date+"23_"+date+"000000";
                     FilterReview(inputFile);
@@ -96,13 +103,11 @@ public class FilterDataUtilTest
 
             fileWriter.flush();
             fileWriter.close();
-            for (int i=0;i<allTimePeriod.length;i++)
+            for (int i = 0; i< arrAllTimePeriod.length; i++)
             {
                 fileWriterList.get(i).flush();
                 fileWriterList.get(i).close();
             }
-
-
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -124,21 +129,27 @@ public class FilterDataUtilTest
             BufferedReader in = null;
             String []reviewInfo = null;
             long time_stamp = 0;
-            String time = null;
-            boolean is_valid = false;
+            String t_when = null;
+            String axis = null;
             String account_id = null;
+            String udid = null;
+            String ip = null;
             String role_id = null;
+            String scene = null;
+            String app_channel = null;
+            String chat_time = null;
             String content = null;
+            boolean is_valid = false;
             String channel = null;
             int fileWriterCount = -1;
 
             if(encoding==null)
             {
-                in = new BufferedReader(new InputStreamReader(bis), 25 * 1024 * 1024);//10M缓存
+                in = new BufferedReader(new InputStreamReader(bis), 25 * 1024 * 1024);//25M缓存
             }
             else
             {
-                in = new BufferedReader(new InputStreamReader(bis,encoding), 25 * 1024 * 1024);//10M缓存
+                in = new BufferedReader(new InputStreamReader(bis,encoding), 25 * 1024 * 1024);//25M缓存
             }
 
             FileWriter fw = new FileWriter(inputFile+"_output.txt");
@@ -147,7 +158,7 @@ public class FilterDataUtilTest
                 if(line==null)
                     continue;
                 reviewInfo = line.split("\t");
-                time = reviewInfo[0];
+                t_when = reviewInfo[0];
                 account_id = reviewInfo[2];
                 role_id = reviewInfo[5];
                 time_stamp = Long.parseLong(reviewInfo[8]);//时间戳
@@ -157,20 +168,20 @@ public class FilterDataUtilTest
                 is_valid = false;
                 countAllReview++;
 
-                for(int i = 0; i< startDate1.length; i++)
+                for(int i = 0; i< arrStartDate1.length; i++)
                 {
-                    if((time_stamp>= startDate1[i]&&time_stamp<= endDate1[i]))
+                    if((time_stamp>= arrStartDate1[i]&&time_stamp<= arrEndDate1[i]))
                     {
                         fileWriterCount = 2*i;
-                        if(userList[i].contains(role_id))
+                        if(arrUserList[i].contains(role_id))
                             is_valid = true;
                         else
                             break;
                     }
-                    else if((time_stamp>= startDate2[i]&&time_stamp<= endDate2[i]))
+                    else if((time_stamp>= arrStartDate2[i]&&time_stamp<= arrEndDate2[i]))
                     {
                         fileWriterCount = 2*i+1;
-                        if(userList[i].contains(role_id))
+                        if(arrUserList[i].contains(role_id))
                             is_valid = true;
                         else
                             break;
@@ -187,7 +198,7 @@ public class FilterDataUtilTest
                 }
 
                 //过滤掉含有屏蔽词的评论（包括特殊语句）
-                Matcher m = p.matcher(content);
+                Matcher m = pattern.matcher(content);
                 if(m.find()) {
                     //System.err.println(channel + "***********" + content+"********"+m.find());
                     continue;
